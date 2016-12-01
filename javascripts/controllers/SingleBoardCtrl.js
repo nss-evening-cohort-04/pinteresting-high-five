a7e4a8235ddc04fa3fa382a5a9688bd0e280a8a0
Mike Teutsch <mgt0117@gmail.com>
2016-11-30 21:11:26 -0600

    individual pins posting in board

--- javascripts/controllers/SingleBoardCtrl.js
+++ javascripts/controllers/SingleBoardCtrl.js
@@ -1,5 +1,57 @@
 "use strict";
 
-app.controller("SingleBoardCtrl", function($scope, $rootScope, $location, BoardFactory){
+app.controller("SingleBoardCtrl", function($scope, $rootScope, $location, $routeParams, BoardFactory, PinFactory){
 	console.log("Hello, this is SingleBoardCtrl.js");
+
+	$scope.currentBoardId = $routeParams.id;
+
+	$scope.pins = [];
+  	$scope.newPinRevealer = false;
+
+//is $rootScope.pins correct?
+  	let getPins = function(){
+    	PinFactory.getPinList().then(function(fbItems){
+      	$scope.pins = fbItems;
+      	console.log("THINGS ARE HAPPENING", fbItems)
+    	});
+  	};
+	getPins();
+
+
+
+	$scope.newPin = {};
+	$scope.addNewPin = function() {
+	        $scope.newPin.uid = $rootScope.user.uid;
+	        PinFactory.postNewPin($scope.newPin).then(function(pinId) {
+	            $location.url("/board/:id");   
+	            $scope.newPin = {};
+	      });
+	  };
+
+
+/*
+ let getBoards = function(){
+    BoardFactory.getBoardList().then(function(fbBoards){
+      $scope.boards = fbBoards;
+      console.log("THINGS ARE HAPPENING", fbBoards)
+    });
+  };
+getBoards();
+*/
+
+
+$scope.deletePin = function(pinId){ //passed this in on click here and in HTML - we delete item.id in html and this is why we created items with ids
+  console.log("you deleted me");
+  PinFactory.deletePin(pinId).then(function(response){
+    getPins(); //once it is deleted then refresh the DOM
+  });
+};
+//get single pin function?
+
+$scope.revealNewPin = function(){
+  $scope.newPinRevealer = true;
+}
+
+
+
 });
